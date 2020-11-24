72f877e0908b27f034087e444c53cc30a470bfad
wonder191 <wonder_191@163.com>
2020-11-24 16:13:24 +0800

    Update ha.py

--- patroni/ha.py
+++ patroni/ha.py
@@ -1013,6 +1013,26 @@ class Ha(object):
             else:
                 # Either there is no connection to DCS or someone else acquired the lock
                 logger.error('failed to update leader lock')
+                
+                # if sync mode, sync node role change from replica to sync_standby
+                # if sync node demote, role change from sync_standby to replica 
+                # when replica promote to sync_standby, callback on_role_change( from replica to sync_standby )
+                # when sync_standby demote to replica, callback on_role_change( from sync_standby to replica )
+                sync_parm = self.cluster.config.data.get('synchronous_mode')
+                if sync_parm:
+                    is_sync = self.is_sync_standby(self.dcs.get_cluster())
+                    if is_sync:
+                        if self.state_handler.role == 'replica':
+                            self.state_handler.set_role('sync_standby')
+                            self.state_handler.call_nowait(ACTION_ON_ROLE_CHANGE)
+                            logger.info("role change from replica to sync_standby")
+                    else:
+                        if self.state_handler.role == 'sync_standby':
+                            self.state_handler.set_role('replica')
+                            self.state_handler.call_nowait(ACTION_ON_ROLE_CHANGE)
+                            logger.info("role change from sync_standby to replica")
+                    logger.info("sync mode is %s, role is %s, is_sync_node is %s", sync_parm, self.state_handler.role, is_sync)
+                
                 if self.state_handler.is_leader():
                     if self.is_paused():
                         return 'continue to run as master after failing to update leader lock in DCS'
